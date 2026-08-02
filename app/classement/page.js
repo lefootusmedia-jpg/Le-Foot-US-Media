a23ec7a775a769000dc218f671ab971885ce6f80
lefootusmedia-jpg <lefootusmedia@gmail.com>
2026-08-02 12:51:26 +0200

    Create page.js

---
+++ app/classement/page.js
@@ -0,0 +1,25 @@
+export default function Classement() {
+  return (
+    <main>
+      <h1>🏈 Power Ranking NFL</h1>
+
+      <h2>Classe ton Top 32</h2>
+
+      <p>
+        Déplace les équipes pour créer ton classement personnel.
+      </p>
+
+      <div>
+        Kansas City Chiefs
+      </div>
+
+      <div>
+        Philadelphia Eagles
+      </div>
+
+      <div>
+        Buffalo Bills
+      </div>
+    </main>
+  );
+}
