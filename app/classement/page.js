58896fc87f2605869129ba98626ed7bea41b3a71
lefootusmedia-jpg <ton-email-github>
2026-08-04 00:16:09 +0200

    Réparation TeamCard

--- app/classement/page.js
+++ app/classement/page.js
@@ -135,22 +135,22 @@ function TeamCard({ team, index, teamLogos }) {
   };
 
 return (
- <div
-  ref={setNodeRef}
-  style={style}
-  {...attributes}
-  {...listeners}
-  className={`team-card ${
-    index === 0
-      ? "gold"
-      : index === 1
-      ? "silver"
-      : index === 2
-      ? "bronze"
-      : ""
-  }`}
-></div>
-</span>
+  <div
+    ref={setNodeRef}
+    style={style}
+    {...attributes}
+    {...listeners}
+    className="team-card"
+  >
+    <span className="rank-number">
+      {index === 0
+        ? "🥇"
+        : index === 1
+        ? "🥈"
+        : index === 2
+        ? "🥉"
+        : `#${index + 1}`}
+    </span>
 
     <img
       src={`/logos/${teamLogos[team]}`}
